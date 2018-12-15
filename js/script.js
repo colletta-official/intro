78b1818d9f08dc4479eef72b86863f80d10e3c0f
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-15 19:18:28 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,56 @@
 var currentScroll=0;
+var offsets=[0];
 $(document).ready(function(){
+	for(var i=1;i<=$("section").length;i++)
+	{
+		offsets[i]=offset[i-1]+$("section").eq(i-1).height();
+	}
 	$("section").each(function () {
 		$(this).on("mousewheel DOMMouseScroll", function (e) {
-			e.preventDefault();
-			var delta = 0;
-			if (!event) event = window.event;
-			if (event.wheelDelta) {
-				delta = event.wheelDelta / 120;
-				if (window.opera) delta = -delta;
-			}
-			else if (event.detail) delta = -event.detail / 3;
-			var moveTop = null;
-			if (delta < 0) {
-				if ($(this).next()[0] != undefined) {
-					moveTop = currentScroll+$(this).outerHeight();
-				}
-			}
-			else {
-				if ($(this).prev()[0] != undefined) {
-					moveTop = currentScroll-$(this).outerHeight();
-				}
-			}
-			if(moveTop != null)
+			if($(document).width()>700)
 			{
-				$(".scrollBlind").stop().animate(
-				{
-					scrollTop: moveTop + 'px'
-					}, {
-					duration: 500, complete: function () {
-						currentScroll=$(".scrollBlind").scrollTop();
+				e.preventDefault();
+				var delta = 0;
+				if (!event) event = window.event;
+				if (event.wheelDelta) {
+					delta = event.wheelDelta / 120;
+					if (window.opera) delta = -delta;
+				}
+				else if (event.detail) delta = -event.detail / 3;
+				var moveTop = null;
+				if (delta < 0) {
+					if ($(this).next()[0] != undefined) {
+						moveTop = currentScroll+$(this).outerHeight();
 					}
-				});
+				}
+				else {
+					if ($(this).prev()[0] != undefined) {
+						moveTop = currentScroll-$(this).outerHeight();
+					}
+				}
+				if(moveTop != null)
+				{
+					$(".scrollBlind").stop().animate(
+					{
+						scrollTop: moveTop + 'px'
+						}, {
+						duration: 500, complete: function () {
+							currentScroll=$(".scrollBlind").scrollTop();
+						}
+					});
+				}
 			}
 		});
 	});
+});
+$(window).scroll(function() {
+	var $el = $('circle');
+	var nth;
+	var scr=$(".scrollBlind").scrollTop();
+	for(nth=0;nth<offsets.length-1;nth++)
+	{
+		if(offsets[nth]<scr && offsets[nth+1]>scr) break;
+	}
+	$el.removeClass('on');
+	$el.eq(nth).addClass('on');
 });
