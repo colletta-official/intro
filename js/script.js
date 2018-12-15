96692ce82891e837bea05d71774db7152d24044c
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-15 17:14:26 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-	console.log("dimigo");
+	$("section").each(function () {
+		$(this).on("mousewheel DOMMouseScroll", function (e) {
+			e.preventDefault();
+			var delta = 0;
+			if (!event) event = window.event;
+			if (event.wheelDelta) {
+				delta = event.wheelDelta / 120;
+				if (window.opera) delta = -delta;
+			}
+			else if (event.detail) delta = -event.detail / 3;
+			var moveTop = null
+			if (delta < 0) {
+				if ($(this).next() != undefined) {
+					moveTop = $(this).next().offset().top;
+				}
+			}
+			else {
+				if ($(this).prev() != undefined) {
+					moveTop = $(this).prev().offset().top;
+				}
+			}
+			$(".scrollBlind").stop().animate(
+			{
+				scrollTop: moveTop + 'px'
+				}, {
+				duration: 500, complete: function () {}
+			});
+		});
+	});
 });
