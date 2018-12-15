c5f6b4eeb5c99d54e09bd1d03ad502213d4b8f3d
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-15 17:36:30 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 			}
 			else if (event.detail) delta = -event.detail / 3;
 			var moveTop = null
-			console.log(this, $(this).next());
+			console.log(this, $(this).next()[0], $(this).prev()[0]);
 			if (delta < 0) {
 				if ($(this).next() != undefined) {
 					moveTop = $(this).next().offset().top;
